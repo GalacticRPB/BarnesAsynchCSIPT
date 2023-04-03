b2a9fc58f6c9b0e0a781351787ab20731fa9a43e
GalacticRPB <russellbarnes18@gmail.com>
2023-04-03 10:45:50 +0800

    added bootstrap, basic layout, font style and font color

--- barnesAsynch/views.py
+++ barnesAsynch/views.py
@@ -6,9 +6,7 @@ def mission(request):
     return render(request,"mission.html")
 
 def vision(request):
-    vision = "In 2030, the Manuel S. Enverga University Foundation is a globally competitive university with high concentrations of talent, excellent teaching environment, rigorous program quality, sufficient resources, and a culture of collaboration."
     return render(request,"vision.html")
 
 def objectives(request):
-    objectives = "MSEUF shall produce graduates who have research-based knowledge, leadership and management skills, and professionalism."
     return render(request,"objectives.html")
